0e0859580503eb3c9b0f0ab9ed2ece8e87ce7af4
hville <hugov@runbox.com>
2022-04-11 08:03:26 -0400

    simpler iframe

--- in-frame.js
+++ in-frame.js
@@ -1,32 +1,33 @@
 const ISMOD = /^[\s]*import[\s'"`*{;]/
+/**
+ * create a function that runs in an iframe
+ * @param {string} code for the iframe exported function
+ * @param {string} [init] initiation code in the iframe
+ * @param {string} [attributes] iframe script tag attributes
+ * @returns {Promise}
+ */
 export function frame(code, init='', attributes=ISMOD.test(init) ? 'type=module' : '') {
+	const uid = '_'+Math.floor(Math.random()*28e11).toString(36)
 
 	// create the iframe - note: no attempts to escape user input
 	const iframeEl = document.createElement('iframe')
-	iframeEl.style.display = 'none' //takes no space vs iframeEl.hidden = true
+	iframeEl.style.display = 'none'
 	iframeEl.sandbox = 'allow-scripts allow-same-origin'
-	iframeEl.srcdoc =`<script ${
-		attributes
+	iframeEl.srcdoc =`<script ${ attributes
 	// complicate access to the parent window:
-	}>Object.defineProperties(window,{parent:{value:window},frameElement:{value:null}});${
-		init
-	};dispatchEvent(new CustomEvent('resolve',{detail:(${
-		code
-	}).bind(this)}))<\/script>`
+	}>Object.defineProperties(window,{parent:{value:window},frameElement:{value:null}});${ init
+	};window.${ uid }=(${ code }).bind(window)<\/script>`
 
 	// get the evaluated code
-	const framed = Promise.all([
-		new Promise( (p,f) => {
-			iframeEl.onload = p
-			iframeEl.onerror = f
-		} ),
-		new Promise( (p,f)=>{
-			document.body.appendChild(iframeEl).contentWindow.addEventListener('resolve', e => {
-				if (typeof e.detail === 'function') p(e.detail)
-				else f(new Error('not a function'))
-			}, {once:true, passive:true})
-		})
-	])
+	const framed = new Promise( (p,f) => {
+		iframeEl.onerror = f
+		iframeEl.onload = () => {
+			const fcn = iframeEl.contentWindow[uid]
+			if (typeof fcn === 'function') p(fcn)
+			else f(new Error('not a function'))
+		}
+		document.body.appendChild(iframeEl)
+	})
 	framed.finally( () => iframeEl.remove() )
-	return framed.then( all => { iframeEl.remove(); return all[1]	} )
+	return framed
 }
