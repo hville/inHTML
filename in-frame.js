950f69dbd8b59e1ac6e2198ea41a0466a20ee8a9
hville <hugov@runbox.com>
2023-03-23 05:44:43 +0900

    in-frame without same-origin

--- in-frame.js
+++ in-frame.js
@@ -4,33 +4,42 @@ const ISMOD = /^[\s]*import[\s'"`*{;]/
  * @param {string} code for the iframe exported function
  * @param {string} [init] initiation code in the iframe
  * @param {string} [attributes] iframe script tag attributes
- * @returns {Promise}
+ * @returns {Promise<function>}
  */
 export function frame(code, init='', attributes=ISMOD.test(init) ? 'type=module' : '') {
-	const uid = '_'+Math.floor(Math.random()*28e11).toString(36)
+	const uid = '_'+Math.floor(Math.random()*Number.MAX_SAFE_INTEGER).toString(36),
+				iframeEl = document.createElement('iframe'),
+				{port1, port2} = new MessageChannel()
 
 	// create the iframe - note: no attempts to escape user input
-	const iframeEl = document.createElement('iframe')
 	iframeEl.style.display = 'none'
-	iframeEl.sandbox = 'allow-scripts allow-same-origin'
-	iframeEl.srcdoc =`<script ${ attributes
-	// complicate access to the parent window:
-	}>Object.defineProperties(window,{parent:{value:window},frameElement:{value:null}});${ init
-	};window.${ uid }=(${ code }).bind(window)<\/script>`
-
-	// get the evaluated code
-	return new Promise( (p,f) => {
+	iframeEl.sandbox = 'allow-scripts'
+	iframeEl.srcdoc = `<script ${ attributes}>${init}; onmessage = ({ports:[port2]}) => {
+	port2.onmessage = evt => {
+		port2.postMessage(window.${ uid }(...evt.data))
+	}
+};
+window.${ uid }=${ code };
+<\/script>`
+	return new Promise( (pass,fail) => {
 		iframeEl.onerror = e => {
-			f(e)
+			fail(e)
 			iframeEl.remove()
 		}
 		iframeEl.onload = () => {
-			const fcn = iframeEl.contentWindow[uid]
-			if (typeof fcn === 'function') {
-				fcn.remove = iframeEl.remove.bind(iframeEl)
-				p(fcn)
+			let ret, err
+			iframeEl.contentWindow.postMessage('', '*', [port2])
+			port1.onmessage = evt => ret(evt.data)
+			port1.onerror = evt => err(evt)
+			function framedFunction(...args) {
+				return new Promise( (p,f) => {
+					ret = p
+					err = f
+					port1.postMessage(args)
+				} )
 			}
-			else f(new Error('not a function'))
+			framedFunction.remove = iframeEl.remove.bind(iframeEl)
+			pass(framedFunction)
 		}
 		document.body.appendChild(iframeEl)
 	})
