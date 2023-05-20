557690c5fe7e714f76cb4328718bee04cf2963c0
hville <hugov@runbox.com>
2023-05-20 17:06:35 -0400

    postmessae transfer

--- in-frame.js
+++ in-frame.js
@@ -1,12 +1,14 @@
 const ISMOD = /^[\s]*import[\s'"`*{;]/
 /**
  * create a function that runs in an iframe
- * @param {string} code for the iframe exported function
- * @param {string} [init] initiation code in the iframe
- * @param {string} [attributes] iframe script tag attributes
- * @returns {Promise<function>}
+ * frame( init, a => {data:a, transfer:[a.buffer]})({data:b, transfer:[b.buffer]}).then( r => console.log(r) )
+ * frame( init, a => a)(b).then( r => console.log(r) )
+ *
+ * @param {string|Function} lambda for the iframe exported function
+ * @param {{context:string, scriptAttributes:string, transfer1:string|Function, transfer2:string|Function}} options
+ * @returns { * => Promise<*> }
  */
-export function frame(code, init='', attributes=ISMOD.test(init) ? 'type=module' : '') {
+export function frame(lambda,	{context='', transfer1, transfer2, scriptAttributes = ISMOD.test(context) ? 'type=module' : ''}={}) {
 	const uid = '$'+Math.floor(Math.random()*(2**(5*6))).toString(32).padStart(6,0),
 				iframeEl = document.createElement('iframe'),
 				{port1, port2} = new MessageChannel()
@@ -14,31 +16,28 @@ export function frame(code, init='', attributes=ISMOD.test(init) ? 'type=module'
 	// create the iframe - note: no attempts to escape user input
 	iframeEl.style.display = 'none'
 	iframeEl.sandbox = 'allow-scripts'
-	iframeEl.srcdoc = `<script ${ attributes}>${init}; onmessage = ({ports:[port2]}) => {
-		port2.onmessage = async evt => port2.postMessage(await window.${ uid }(...evt.data))
-};
-window.${ uid }=${ code };
-<\/script>`
-	return new Promise( (pass,fail) => {
-		iframeEl.onerror = e => {
-			fail(e)
-			iframeEl.remove()
-		}
-		iframeEl.onload = () => {
-			let ret, err
-			iframeEl.contentWindow.postMessage('', '*', [port2])
-			port1.onmessage = evt => ret(evt.data)
-			port1.onerror = evt => err(evt)
-			function framedFunction(...args) {
-				return new Promise( (p,f) => {
-					ret = p
-					err = f
-					port1.postMessage(args)
-				} )
-			}
-			framedFunction.remove = iframeEl.remove.bind(iframeEl)
-			pass(framedFunction)
-		}
-		document.body.appendChild(iframeEl)
+	iframeEl.srcdoc = `<script ${ scriptAttributes }>${ context
+		}; const ${ uid }={ f:${ lambda.toString() }, t:${ transfer2?.toString()
+		} }; onmessage = ({ports:[port2]}) => { port2.onmessage = async evt => { const result = await ${ uid
+		}.f(...evt.data); port2.postMessage( result${ transfer2 ? `, ${ uid }.t(result)` : ''
+		} ) } }<\/script>`
+
+	const framed = new Promise( (p,f) => {
+		iframeEl.onload = p
+		iframeEl.onerror = f
+	}).then( () => {
+		iframeEl.contentWindow.postMessage('', '*', [port2])
 	})
+
+	async function framedFunction(...args) {
+		await framed
+		return  new Promise( (p,f) => {
+			port1.onmessage = evt => p(evt.data)
+			port1.onerror = f
+			port1.postMessage(args, transfer1?.(...args) || [])
+		} )
+	}
+	framedFunction.remove = iframeEl.remove.bind(iframeEl)
+	document.body.appendChild(iframeEl)
+	return framedFunction
 }
